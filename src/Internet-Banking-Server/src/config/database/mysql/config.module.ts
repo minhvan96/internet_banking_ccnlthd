77dfa1ce58890ebe4ad1529291d1162eb66a9803
minhvan <minhvan.996@outlook.com>
2022-11-22 16:57:45 +0700

    FEAT - init config

---
+++ src/Internet-Banking-Server/src/config/database/mysql/config.module.ts
@@ -0,0 +1,3 @@
+import { ConfigModule } from '@nestjs/config';
+
+export class MysqlConfigModule extends ConfigModule {}
---
+++ src/Internet-Banking-Server/src/config/database/mysql/config.service.ts
@@ -0,0 +1,7 @@
+export class MysqlConfigService {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}
