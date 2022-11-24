b7f8f8ad4c1510e208e582efa87c6ebe117c320f
minhvan <minhvan.996@outlook.com>
2022-11-24 13:15:25 +0700

    FEAT - init authentication strategy

---
+++ src/Internet-Banking-Server/src/authentication/strategies/access-token.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { JwtConstants } from '../../common/constants/jwt-constants';
+
+type JwtPayload = {
+  sub: string;
+  username: string;
+  time: Date;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JwtConstants.secret,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return { username: payload.username, time: payload.time };
+  }
+}
