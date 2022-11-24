7408b4fc3d4dde96301dd8cff27ef13894d4a53e
minhvan <minhvan.996@outlook.com>
2022-11-24 21:33:47 +0700

    FEAT - update auth controller

--- src/Internet-Banking-Server/src/authentication/auth.controller.ts
+++ src/Internet-Banking-Server/src/authentication/auth.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { Request } from 'express';
+import { RefreshTokenGuard } from './guards/refreshToken.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CommandBus } from '@nestjs/cqrs';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly commandBus: CommandBus,
+              private readonly authService: AuthService) {
+
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  @ApiOperation({ summary: 'This API is user login' })
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @Get('refresh')
+  @UseGuards(RefreshTokenGuard)
+  refreshTokens(@Req() req: Request) {
+    const { user } = req;
+    const userName = user['username'];
+    const refreshToken = req.user['refreshToken'];
+    return this.authService.refreshTokens(userName, refreshToken);
+  }
+}
