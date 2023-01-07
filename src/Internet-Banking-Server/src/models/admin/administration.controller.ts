3bb2b43005758c7da21f7500e25145cbc7a7e444
ThanhPhu-Dev <tphu2000@gmail.com>
2023-01-07 21:02:40 +0700

    document for swagger

--- src/Internet-Banking-Server/src/models/admin/administration.controller.ts
+++ src/Internet-Banking-Server/src/models/admin/administration.controller.ts
@@ -1,5 +1,5 @@
 import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
-import {ApiBearerAuth, ApiTags} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiOperation, ApiTags} from "@nestjs/swagger";
 import {AccessTokenGuard} from "../../auth/guards/access-token.guard";
 import {CommandBus, QueryBus} from "@nestjs/cqrs";
 import {CreateEmployeeRequest, CreateOrUpdateEmployeeCommand} from "./commands/create-or-update-employee.command";
@@ -13,6 +13,7 @@ export class AdministrationController {
     ) {
     }
     @Post("/create-or-update-employee")
+    @ApiOperation({ summary: "create employee" })
     @ApiBearerAuth()
     @UseGuards(AccessTokenGuard)
     async createUpdateEmployee(@Body() request: CreateEmployeeRequest){
@@ -21,6 +22,7 @@ export class AdministrationController {
 
     @Post("/delete-employee/:id")
     @ApiBearerAuth()
+    @ApiOperation({ summary: "Delete employee" })
     @UseGuards(AccessTokenGuard)
     async deleteEmployee(@Param('id') employeeId: number){
         return await this.commandBus.execute(new DeleteEmployeeCommand(employeeId))
@@ -28,6 +30,7 @@ export class AdministrationController {
 
     @Get("/get-employee")
     @ApiBearerAuth()
+    @ApiOperation({ summary: "Get employee" })
     @UseGuards(AccessTokenGuard)
     async getEmployee(){
         return await this.queryBus.execute(new GetEmployeeQuery());
