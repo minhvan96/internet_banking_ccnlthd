7d7d7d9f7a8267c54d4d3fb614f42bc8604156a0
minhvan96 <74522363+minhvan96@users.noreply.github.com>
2023-01-07 17:00:43 +0700

    FEAT - update register customer

--- src/internet-banking-client/src/apis/customerApi.js
+++ src/internet-banking-client/src/apis/customerApi.js
@@ -1,2 +1,14 @@
 import apiInstance from "./config";
 
+export const registerCustomer = async (body) => {
+  try {
+    body.id = 0;
+    const { data } = await apiInstance.post(
+      "auth/register",
+      body
+    );
+    return data;
+  } catch (error) {
+    throw new Error("add failed");
+  }
+};
