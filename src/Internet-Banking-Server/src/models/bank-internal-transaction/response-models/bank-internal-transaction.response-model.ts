d5a20ccd192fc7344a432cfe0a4b81a028067a77
minhvan96 <74522363+minhvan96@users.noreply.github.com>
2023-01-07 19:41:07 +0700

    FEAT - update bank internal transaction response

--- src/Internet-Banking-Server/src/models/bank-internal-transaction/response-models/bank-internal-transaction.response-model.ts
+++ src/Internet-Banking-Server/src/models/bank-internal-transaction/response-models/bank-internal-transaction.response-model.ts
@@ -1,16 +1,36 @@
-import { BankTransactionPaymentType } from '../../../entities/enums/bank-transaction-payment-type.enum';
+import { BankTransactionPaymentType } from "../../../entities/enums/bank-transaction-payment-type.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class BankInternalTransactionResponseModel {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   transferFromAccount: string;
+
+  @ApiProperty()
   transferToAccount: string;
+
+  @ApiProperty()
   transferToName: string;
+
+  @ApiProperty()
   transferToBeneficiary: string;
+
+  @ApiProperty()
   transferAmount: number;
+
+  @ApiProperty()
   description: string;
-  fee: number
+
+  @ApiProperty()
+  fee: number;
+
+  @ApiProperty()
   paymentType: BankTransactionPaymentType;
-  transferDate: Date
+
+  @ApiProperty()
+  transferDate: Date;
 
   constructor(
     id: number,
